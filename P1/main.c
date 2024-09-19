1626bf11e3f63d025bb8dc857ded3d5b63358c51
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-09-19 09:12:28 +0200

    Cabeceras funciones

--- P1/main.c
+++ P1/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+void test();
+
+int fib1(int n);
+
+int fib2(int n);
+
+int fib3(int n);
+
 int main(void) {
-    printf("Hello, World!\n");
+    test();
     return 0;
+}
+
+void test(){
+
+}
+
+int fib1(int n){
+
+}
+
+int fib2(int n){
+
+}
+
+int fib3(int n){
+
 }
