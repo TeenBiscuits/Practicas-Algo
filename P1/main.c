9f7124f261c4336bb2c6d1d86705a03783f233c4
paablomiguez <pablommui@gmail.com>
2024-09-19 09:22:07 +0200

    Update main.c

--- P1/main.c
+++ P1/main.c
@@ -22,9 +22,24 @@ int fib1(int n){
 }
 
 int fib2(int n){
-
+int i=1;
+int j=0;
+    for (int k = 0; k < n; ++k) {
+        j = i+j;
+        i = j-i;
+    }
+    return j;
 }
 
 int fib3(int n){
 
 }
+
+
+
+
+
+
+
+
+
