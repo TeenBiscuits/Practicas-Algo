8b95163b7e812f1c3b34574203f371110a4cddfe
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-09-26 11:02:20 +0200

    Arreglillos

--- P1/main.c
+++ P1/main.c
@@ -12,34 +12,28 @@ void test();
 
 double microsegundos();
 
-int selectalgo(int n, int algo);
+int selectalgo(const int n, const int algo);
 
-int fib1(int n);
+int fib1(const int n);
 
-int fib2(int n);
+int fib2(const int n);
 
 int fib3(int n);
 
-double tiempospetit(int n, int alg);
+double tiempospetit(const int n, const int alg);
 
 int main(void) {
-    // Impresión en archivo
-    if (guardaraarchivo()) {
-        // Test Function
-        test();
-    }
+    if (guardaraarchivo()) test();
     return 0;
 }
 
 bool guardaraarchivo() {
-    int fd;
-    char *name = "tiempos.txt";
-    fd = open(name, O_WRONLY | O_CREAT, 0644);
+    const char *name = "tiempos.txt";
+    const int fd = open(name, O_WRONLY | O_CREAT, 0644);
     if (fd == -1) {
         perror("open failed");
         return false;
     }
-
     if (dup2(fd, 1) == -1) {
         perror("dup2 failed");
         return false;
@@ -49,58 +43,49 @@ bool guardaraarchivo() {
 
 void test() {
     int n = 1;
-    int n1[5] = {2, 4, 8, 16, 32};
-    int n2[5] = {1000, 10000, 100000, 1000000, 10000000};
-
-    printf("Test de funciones de Fibonacci\n"
+    const int n1[5] = {2, 4, 8, 16, 32},
+            n2[5] = {1000, 10000, 100000, 1000000, 10000000};
+    printf("Algoritmos de Fibonacci   -  "
         "Por Pablo Portas, Pablo Míguez y Maite González\n");
     printf("función\t       n         tiempo"
         "   sqrt(log(n))         log(n)         n**0.5\n"
         "---------------------------------------------"
         "-------------------------------\n");
-
     char *nombref = "fib";
-    double t1, t2, t, x, y, z;
-
     for (int alg = 1; alg <= 3; alg++) {
         for (int i = 0; i < 5; i++) {
             if (alg == 1) {
                 nombref = "fib1";
                 n = n1[i];
-            }
-            if (alg == 2) {
-                nombref = "fib2";
+            } else {
                 n = n2[i];
+                if (alg == 2) nombref = "fib2";
+                if (alg == 3) nombref = "fib3";
             }
-            if (alg == 3) {
-                nombref = "fib3";
-                n = n2[i];
-            }
-            t1 = microsegundos();
+            const double t1 = microsegundos();
             selectalgo(n, alg);
-            t2 = microsegundos();
-            t = t2 - t1;
+            const double t2 = microsegundos();
+            double t = t2 - t1;
             if (t <= 500) {
                 t = tiempospetit(n, alg);
             }
-            x = t / sqrt(log(n));
-            y = t / log(n);
-            z = t / pow(n, 0.5);
+            const double x = t / sqrt(log(n)), y = t / log(n),
+                    z = t / pow(n, 0.5);
             printf("%s%12d%15.3f%15.6f%15.6f%15.6f\n", nombref, n, t, x, y, z);
         }
     }
-
-    printf("----------------------------------------------------------------------------\n");
+    printf("---------------------------------------------"
+        "-------------------------------\n");
 }
 
-int selectalgo(int n, int algo) {
+int selectalgo(const int n, const int algo) {
     if (algo == 1) return fib1(n);
     if (algo == 2) return fib2(n);
     if (algo == 3) return fib3(n);
     return 0;
 }
 
-int fib1(int n) {
+int fib1(const int n) {
     if (n < 2) {
         return n;
     } else {
@@ -108,7 +93,7 @@ int fib1(int n) {
     }
 }
 
-int fib2(int n) {
+int fib2(const int n) {
     int i = 1, j = 0;
     for (int k = 0; k < n; ++k) {
         j = i + j;
@@ -133,20 +118,19 @@ int fib3(int n) {
     return j;
 }
 
-double tiempospetit(int n, int alg) {
-    double t, ta, tb; int k;
+double tiempospetit(const int n, const int alg) {
+    double t = 0, k;
     for (k = 1; t < 500; k = k * 2) {
-        ta = microsegundos();
+        const double ta = microsegundos();
         for (int i = k; i > 0; i--) selectalgo(n, alg);
-        tb = microsegundos();
+        const double tb = microsegundos();
         t = tb - ta;
     }
-    return t / (double) k;
+    return t / k;
 }
 
 double microsegundos() {
     struct timeval t;
-    if (gettimeofday(&t,NULL) < 0)
-        return 0.0;
+    if (gettimeofday(&t,NULL) < 0) return 0.0;
     return (t.tv_usec + t.tv_sec * 1000000.0);
 }
