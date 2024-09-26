3b911034e2f7fe724f7bab61af8c78f7ca026e53
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-09-26 09:21:28 +0200

    Código Listo
    Muchas cosas soy muy listo

--- P1/main.c
+++ P1/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/time.h>
 
 void test();
@@ -13,42 +16,77 @@ int fib2(int n);
 int fib3(int n);
 
 int main(void) {
+    // Impresión en archivo
+    int fd;
+    char *name = "tiempos.txt";
+    fd = open(name, O_WRONLY | O_CREAT, 0644);
+    if (fd == -1) {
+        perror("open failed");
+        return 1;
+    }
+
+    if (dup2(fd, 1) == -1) {
+        perror("dup2 failed");
+        return 1;
+    }
+
+    // Test Function
     test();
+
     return 0;
 }
 
 void test(){
-    int n;
-
+    int n1[5] = {2,4,8,16,32};
+    int n2[5] = {1000,10000,100000,1000000,10000000};
     puts("Test de funciones de Fibonacci");
-    printf("Valor de n: ");
-    scanf("%d",&n);
+    puts("Por Pablo Portas, Pablo Míguez y Maite González");
+    puts("");
+
+    printf("función\t       n\t tiempo   sqrt(log(n))\t       log(n)\t      n**0.5\n");
+    printf("----------------------------------------------------------------------------\n");
 
     double t1, t2, t, x, y, z;
-    t1 = microsegundos();
-    fib1(n);
-    t2 = microsegundos();
-    t = t2-t1;
-    x = t / sqrt(log(n));
-    y = t / log(n);
-    z = t / pow(n, 0.5);
-    printf("fib1%12d%15.3f%15.6f%15.6f%15.6f\n", n, t, x, y, z);
-    t1 = microsegundos();
-    fib2(n);
-    t2 = microsegundos();
-    t = t2-t1;
-    x = t / sqrt(log(n));
-    y = t / log(n);
-    z = t / pow(n, 0.5);
-    printf("fib2%12d%15.3f%15.6f%15.6f%15.6f\n", n, t, x, y, z);
-    t1 = microsegundos();
-    fib3(n);
-    t2 = microsegundos();
-    t = t2-t1;
-    x = t / sqrt(log(n));
-    y = t / log(n);
-    z = t / pow(n, 0.5);
-    printf("fib3%12d%15.3f%15.6f%15.6f%15.6f\n", n, t, x, y, z);
+
+    // Algoritmo Fib 1
+    // Mediciones con valores {2,4,8,16,32}
+    for(int i = 0; i < 5; i++) {
+        t1 = microsegundos();
+        fib1(n1[i]);
+        t2 = microsegundos();
+        t = t2-t1;
+        x = t / sqrt(log(n1[i]));
+        y = t / log(n1[i]);
+        z = t / pow(n1[i], 0.5);
+        printf("fib1%12d%15.3f%15.6f%15.6f%15.6f\n", n1[i], t, x, y, z);
+    }
+
+    // Algoritmo Fib2
+    // Mediciones con valores {1000,10000,100000,1000000,10000000}
+    for(int i = 0; i < 5; i++) {
+        t1 = microsegundos();
+        fib2(n2[i]);
+        t2 = microsegundos();
+        t = t2-t1;
+        x = t / sqrt(log(n2[i]));
+        y = t / log(n2[i]);
+        z = t / pow(n2[i], 0.5);
+        printf("fib2%12d%15.3f%15.6f%15.6f%15.6f\n", n2[i], t, x, y, z);
+    }
+
+    // Algoritmo Fib3
+    // Mediciones con valores {1000,10000,100000,1000000,10000000}
+    for(int i = 0; i < sizeof(n2); i++) {
+        t1 = microsegundos();
+        fib3(n2[i]);
+        t2 = microsegundos();
+        t = t2-t1;
+        x = t / sqrt(log(n2[i]));
+        y = t / log(n2[i]);
+        z = t / pow(n2[i], 0.5);
+        printf("fib3%12d%15.3f%15.6f%15.6f%15.6f\n", n2[i], t, x, y, z);
+    }
+    printf("----------------------------------------------------------------------------\n");
 }
 
 int fib1(int n){
