ebdfe5ccb894349ecf2fb35e35779ec9486bf9bb
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-10-10 09:27:14 +0200

    Funciones Declaradas

--- P2/main.c
+++ P2/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+void ord_ins (int v[], int n);
+
+void ord_rap_ins (int v[], int iz, int dr);
+
+void ord_rap (int v[], int iz, int dr);
+
 int main(void) {
     printf("Hello, World!\n");
     return 0;
+}
+
+void ord_ins (int v[], int n) {
+    /* ... */
+}
+
+void ord_rap_ins (int v[], int iz, int dr) {
+    /* ... */
+}
+
+void ord_rap (int v[], int iz, int dr) {
+    /* ... */
 }
