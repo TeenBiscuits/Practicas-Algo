d254f9f760455e5a42d4576cc9d104d2449830fa
maiteegonzaalez <maite.gonzalez@udc.es>
2024-10-14 19:32:01 +0200

    función ordenación por inserción lista (la auxiliar rapida no la entiendo)

--- P2/main.c
+++ P2/main.c
@@ -12,11 +12,25 @@ int main(void) {
 }
 
 void ord_ins (int v[], int n) {
-    /* ... */
+    int x = 0;
+    int j = 0;
+
+    for (int i = 1; i < n; i++) {
+        x = v[i];
+        j = i - 1;
+
+        while (j >= 0 && v[j] > x) {
+            v[j + 1] = v[j];
+            j = j - 1;
+        }
+        v[j + 1] = x;
+    }
 }
 
 void ord_rap_aux (int v[], int iz, int dr) {
-    /* ... */
+    if (iz < dr) {
+
+    }
 }
 
 void ord_rap (int v[], int n) {
