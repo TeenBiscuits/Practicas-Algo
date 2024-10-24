5c71882deefd1a35536c5ebb2471b6d47892d903
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-10-24 09:16:42 +0200

    Ahora mola más

--- P3/main.c
+++ P3/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+bool guardaraarchivo(const char *archivo);
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    if (guardaraarchivo("tiempos.txt")) {
+        printf("<--- Práctica de Pablos & Maite --->\n");
+    }
+    else return EXIT_FAILURE;
+    return EXIT_SUCCESS;
+}
+
+bool guardaraarchivo(const char *archivo) {
+    int file = open(archivo, O_WRONLY | O_CREAT, 0644);
+    if (file == -1) {
+        perror("open failed");
+        return false;
+    }
+    if (dup2(file, 1) == -1) {
+        perror("dup2 failed");
+        return false;
+    }
+    return true;
 }
