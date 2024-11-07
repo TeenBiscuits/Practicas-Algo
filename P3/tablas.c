a9107bcd32a66d6e69a8c402da2945a0ec549401
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-11-07 10:32:08 +0100

    Faltaban librerías

--- P3/tablas.c
+++ P3/tablas.c
@@ -1,6 +1,11 @@
 // Creado por Pablo P. Pablo M. Maite G.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
 #include "tablas.h"
 
